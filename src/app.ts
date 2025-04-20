e672acebd7cb61983709ecfb89f277a67a349c3e
Abdullah Al Mamun <bdmamun100@gmail.com>
2025-04-21 00:19:57 +0600

    authentication bug fixed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import router from './app/routes';
 import notFound from './app/middlewares/notFound';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
@@ -8,7 +9,15 @@ const app: Application = express();
 
 // Parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173', // Local development
+    ],
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 
 // Application routes
 app.use('/api', router);
