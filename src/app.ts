fbffcc71561ea611e08750c146f33195d4359e37
Abdullah Al Mamun <bdmamun100@gmail.com>
2025-04-25 23:55:46 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['https://spin-cycle24.vercel.app'],
+    origin: ['https://spin-cycle24.vercel.app', 'http://localhost:5173'],
     credentials: true,
   }),
 );
